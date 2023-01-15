a15c75264441c9f4a71cae8b239b162eddb9d4b4
mitsuru-kasai <mitsuru.kasai@loov.co.jp>
2023-01-15 18:11:40 +0900

    Expression を使った

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,53 @@
+import exp from "constants";
 import { useState } from "react";
+import { isExpressionStatement } from "typescript";
 import "./App.css";
 
+type Operation = (a: number, b: number) => number;
+type Expression = [Expression, Operation, number] | number;
+type Inputs = [ex: Expression, op?: Operation];
+
 function App() {
-  const [inputs, setInputs] = useState<PadValue[]>([0]);
-  const lastInput = inputs.at(-1);
+  const [inputs, setInputs] = useState<Inputs>([0]);
 
   let display: number;
-  if (typeof lastInput === "number") {
+  if (!inputs[1]) {
     // 最終入力が数字の場合は、入力中の数字を表示する
-    display = lastInput;
+    const expression = inputs[0];
+    display = typeof expression === "number" ? expression : expression[2];
   } else {
-    // 最終入力がオペレーションの場合は、過去の計算結果を表示する
-    if (typeof inputs[0] !== "number")
-      throw Error("inputs are something wrong");
-
-    // [number, operation, number, operation, ...]
-    // と並んでいるはずなので、順番に operation(currentTotal, nextNumber) を計算していく
-    let currentTotal = inputs[0];
-    let pointer = 1;
-    while (true) {
-      const operation = inputs[pointer];
-      const nextNumber = inputs[pointer + 1];
-      if (!operation === undefined || nextNumber === undefined) {
-        break;
-      } else if (
-        typeof operation !== "function" ||
-        typeof nextNumber !== "number"
-      ) {
-        throw Error("inputs are something wrong");
-      }
-
-      currentTotal = operation(currentTotal, nextNumber);
-      pointer += 2;
-    }
-    display = currentTotal;
+    // 最終入力がオペレーションの場合は、これまでの計算を評価して表示する
+    display = evaluate(inputs[0]);
   }
 
   function onClickNumber(number: number) {
-    if (typeof lastInput === "number") {
-      setInputs([...inputs.slice(0, -1), lastInput * 10 + number]);
-    } else {
-      setInputs([...inputs, number]);
+    if (inputs[1]) {
+      return setInputs([[inputs[0], inputs[1], number]]);
     }
+
+    const expression = inputs[0];
+    if (typeof expression === "number") {
+      return setInputs([expression * 10 + number]);
+    }
+
+    setInputs([[expression[0], expression[1], expression[2] * 10 + number]]);
   }
 
   function onClickOperation(operation: Operation) {
-    if (typeof lastInput === "number") {
-      setInputs([...inputs, operation]);
-    } else {
-      setInputs([...inputs.slice(0, -1), operation]);
+    setInputs([inputs[0], operation]);
+  }
+
+  function onClickClear() {
+    if (inputs[1]) {
+      return setInputs([inputs[0]]);
     }
+
+    const expression = inputs[0];
+    if (typeof expression === "number") {
+      return setInputs([0]);
+    }
+
+    setInputs([expression[0], expression[1]]);
   }
 
   function createNumberPad(number: number) {
@@ -70,7 +68,7 @@ function App() {
         </tr>
         <tr>
           <td>
-            <Pad symbol="C" onClick={() => setInputs(inputs.slice(0, -1))} />
+            <Pad symbol="C" onClick={() => onClickClear()} />
           </td>
           <td>
             <Pad symbol="AC" onClick={() => setInputs([0])} />
@@ -106,8 +104,18 @@ function App() {
   );
 }
 
-type Operation = (a: number, b: number) => number;
-type PadValue = number | Operation;
+function evaluate(expression: Expression): number {
+  if (typeof expression === "number") {
+    return expression;
+  }
+
+  const exp = expression[0];
+  const operation = expression[1];
+  const num = expression[2];
+
+  return operation(evaluate(expression[0]), num);
+}
+
 type PadProps<T> =
   | {
       // value を受け取る場合は onClick は第一引数をとり、symbol は省略可能
